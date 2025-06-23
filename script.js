47df64e127d846ed1d74377941b35466d386c7ce
Alreynn <aliung04@gmail.com>
2025-06-23 23:22:22 +0700

    Merubah algoritma dari start message, dan ada fungsi yang ditambahkan.

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+const textarea = document.getElementById('textarea');
+textarea.value =
+    'Antarmuka Baris Perintah buat orang gabut.\nKode sumber:    https://github.com/Alreynn/cli-untuk-gabut\nSitus:          https://alreynn.github.io/cli-untuk-gabut/\nSemua fungsi di dalam Antarmuka Baris Perintah ini peka huruf besar/kecil.\n';
+
 function sendMessage() {
-    const textarea = document.getElementById('textarea');
     const input = document.getElementById('input');
     const userInput = input.value;
     
-    const selectedWord = [ 'pkg', 'apt', 'git', 'sudo', 'cd', 'ls', 'rm' ]
+    const selectedWord = [
+        'pkg', 'apt', 'git', 'sudo', 'cd',
+        'ls', 'rm', 'touch', 'mkdir', 'cp',
+        'rmdir', 'cat', 'pwd', 'mv', 'find',
+        'vim', 'nano', 'nvim', 'python'
+    ]
+    const bypassedWord = [ 'echo' ]
     textarea.value += `$ ${userInput}\n`;
     input.value = '';
     
     selectedWord.forEach(wordIncluded => {
         if (userInput.includes(wordIncluded)) {
-            textarea.value += 'Tidak didukung!\n';
+            textarea.value += `${wordIncluded} tidak didukung!\n`;
         }
     })
+    
+    if (userInput.includes(bypassedWord)) {
+        textarea.value += 'Fungsi ini sedang dalam pengerjaan!\n';
+    } else if (userInput === 'refresh') {
+        window.location.reload();
+        textarea.value += 'Menyegarkan...\n';
+    } else if (userInput === 'clear') {
+        textarea.value = '';
+    }
     return false;
 }
