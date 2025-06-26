50d5f3c0c11824d4981e95ce712bb2abd9c41408
Alreynn <aliung04@gmail.com>
2025-06-26 22:44:34 +0700

    Penambahan fitur pemindaha  penutup aside (aside sandwich)

--- script.js
+++ script.js
@@ -1,58 +1,70 @@
 const textarea = document.getElementById('textarea');
 const input = document.getElementById('input');
-textarea.value =
+const asideSandwich = document.getElementById('asideSandwich');
+textarea.value = // Teks awal pada saat pertama bika
     'Antarmuka Baris Perintah buat orang gabut.\nKode sumber:    https://github.com/Alreynn/cli-untuk-gabut\nSitus:          https://alreynn.github.io/cli-untuk-gabut/\nMasih dalam tahap pengerjaan.\n';
 
 function sendMessage() {
     const userInput = input.value;
-    const selectedWord = [
-        'pkg', 'apt', 'git', 'sudo', 'cd',
-        'ls', 'rm', 'touch', 'mkdir', 'cp',
-        'rmdir', 'cat', 'pwd', 'mv', 'find',
-        'vim', 'nano', 'nvim', 'python'
+    const selectedWord = [ // Kata - kata yang dipilih buat ngeluarin output tertentu.
+        'pkg', 'apt', 'git', 'sudo', 'cd', 'ls',
+        'rm', 'touch', 'mkdir', 'mv', 'cp', 'rmdir',
+        'cat', 'pwd', 'mv', 'find', 'vim', 'nano',
+        'nvim', 'python'
     ]
-    const bypassedWord = [ 'echo' ]
+    const bypassedWord = [ 'echo', 'changelog' ]
     if (userInput !== '') {     // Cek kondisi apabila userInput tidak kosong
         textarea.value += `$ ${userInput}\n`;
         input.value = '';
-    }
-    
-    selectedWord.forEach(wordIncluded => {
-        if (userInput.includes(wordIncluded)) {
-            textarea.value += `${wordIncluded} tidak didukung!\n`;
-        }
-    })
-    if (userInput.includes(bypassedWord)) {
-        textarea.value += 'Fungsi ini sedang dalam pengerjaan!\n';
     } else if (userInput === 'refresh') {
         window.location.reload();
         textarea.value += 'Menyegarkan...\n';
     } else if (userInput === 'clear') {
         textarea.value = '';
     }
+    selectedWord.forEach(wordIncluded => {      // Mengecek input kata per kata untuk memastikan apakah mengandung kata yang termasuk dalam array 'selectedWord'
+        if (userInput.includes(wordIncluded)) {
+            textarea.value += `${wordIncluded} tidak didukung!\n`;
+        }
+    })
+    bypassedWord.forEach(wordBypass => {
+        if (userInput.includes(wordBypass)) {
+            textarea.value += 'Fungsi ini sedang dalam pengerjaan!\n';
+        }
+    })
     return false;
 }
 
-function getFontSize() {
+function getLocalStorage() {
     const textareaSize = localStorage.getItem('fontSize');
+    const closeAsidePos = localStorage.getItem('closeAsidePos');
     textarea.style.fontSize = textareaSize;
+    asideSandwich.style.top = closeAsidePos;
 }
 function closeAside() {
-    const aside = document.getElementById('aside');
-    aside.classList.toggle('show');
+    document.getElementById('aside').classList.toggle('show');
 }
-function showOption() {
-    const fontSizeOption = document.getElementById('fontSizeOption');
-    fontSizeOption.classList.toggle('show');
+function showOption(options) {
+    document.getElementById(options).classList.toggle('show');
 }
 function resize(size) {
     textarea.style.fontSize = size;
     localStorage.setItem('fontSize', textarea.style.fontSize);
-    getFontSize();
+    getLocalStorage();
 }
-getFontSize();
-
+function changeCloseAsidePos(pos) {
+    const position = asideSandwich.style.top = pos;
+    localStorage.setItem('closeAsidePos', position);
+    getLocalStorage();
+}
 textarea.addEventListener('click', () => {
-    aside.classList.remove('show');
-    fontSizeOption.classList.remove('show');
+    const closeOptions = [
+        aside, fontSizeOption,
+        closeAsidePos, changeAsidePos
+    ];
+    closeOptions.forEach((item) => {
+        item.classList.remove('show');
+    });
 })
+
+getLocalStorage();
